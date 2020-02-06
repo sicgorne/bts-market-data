adc98eb8558f3d637fee576d30cc982efca5d3a4
Nejc Goršič <nejcgorsic1@gmail.com>
2020-02-06 13:52:15 +0100

    Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   /**
-   * 1. Integrate Bootstrap 4 framework to your project.
-   * 2. Create a login form (write HTML in app.component.html and CSS to app.component.css)
-   * 3. Write a public function named authenticate and bind a click event from login submit button to it.
-   * 4. Bind username and password from login form to new public properties (username and password). (hint: use  [(ngModel)] directive)
-   * 5. Print username and password via console.log() inside your brand new authenticate function.
-   */
-  public title = 'bts-market-data';
+ * 1. Integrate Bootstrap 4 framework to your project.
+ * 2. Create a login form (write HTML in app.component.html and CSS to app.component.css)
+ * 3. Write a public function named authenticate and bind a click event from l-ogin submit button to it.
+ * 4. Bind username and password from login form to new public properties (username and password). (hint: use  [(ngModel)] directive)
+ * 5. Print username and password via console.log() inside your brand new authenticate function.
+ */
+  public username: string;
+  public password: string;
 
-  public master: string;
+  authenticate() {
 
-  public people = [
-    'Janey', 'Miha', 'Rojc', 'Smrekar'
-  ];
-
-  public showTheMaster(): void {
-    console.log('Showing the master ...');
-    this.master = 'Jan';
   }
 }
