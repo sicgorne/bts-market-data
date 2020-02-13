7249382d587f4a0c7581b84b354801d47efa19b6
Nejc Goršič <nejcgorsic1@gmail.com>
2020-02-13 13:49:49 +0100

    Finished second part

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,5 +2,7 @@ export class User {
   public firstName: string;
   public lastName: string;
   public email: string;
+  public email2: string;
   public password: string;
+  public password2: string;
 }
--- src/app/modules/register/register.module.ts
+++ src/app/modules/register/register.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,7 +10,8 @@ import { RegisterComponent } from './register.component';
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    FormsModule
   ]
 })
 export class RegisterModule { }
